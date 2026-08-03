b45de97328e3a586387f348bec7fc6ac2c636a82
LFTE-guy <LFTE@proton.me>
2026-08-03 09:25:28 -0400

    basic packet inspection + TTL option

--- prjct.py
+++ prjct.py
@@ -1,13 +1,45 @@
-from scapy.all import scapy,IP,sniff,TCP, UDP, ARP
+from scapy.all import scapy,IP,sniff,TCP, UDP, ARP, Ether
 print("welcome to ali filter")
+SHOW_TTL = input("ttl?:")
+def showttl(packet):
+	if SHOW_TTL == "y":
+		return packet[IP].ttl
+	else:
+		pass
+def flagging(packet):
+	flag = int(packet[TCP].flags)
+	if flag == 2:
+		print(f"\033[1;32m[+] NEW CONNECTION REQUEST (SYN)\033[0m")
+	elif flag == 18:
+		print(f"\033[1;35m[+] HANDSHAKE RESPONSE (SYN-ACK)\033[0m")
+	elif flag == 16:
+		print(f"\033[0;32m[+] PACKET ACKNOWLEDGED (ACK)\033[0m")
+	elif flag == 8:
+		print(f"\033[0;36m[*] DATA PUSHED TO APP (PSH)\033[0m")
+	elif flag == 24:
+		print(f"\033[1;36m[*] ACTIVE DATA STREAM (PSH-ACK)\033[0m")
+	elif flag == 1:
+		print(f"\033[1;33m[-] CONNECTION FINISHED (FIN)\033[0m")
+	elif flag == 17:
+		print(f"\033[1;33m[-] TEARDOWN ACKNOWLEDGED (FIN-ACK)\033[0m")
+	elif flag == 64:
+		print(f"\033[0;33m[!] CONGESTION NOTIFICATION (ECE)\033[0m")
+	elif flag == 128:
+		print(f"\033[0;33m[!] CONGESTION WINDOW REDUCED (CWR)\033[0m")
+	elif flag == 4:
+		print(f"\033[1;31m[!] CONNECTION RESET / ABORTED (RST)\033[0m")
+	elif flag == 20:
+		print(f"\033[1;31m[!] CONNECTION RESET ACKNOWLEDGED (RST-ACK)\033[0m")
+	elif flag == 32:
+		print(f"\033[1;31m[!] URGENT DATA POINTER (URG)\033[0m")
 def inspect(packet):
 	if packet.haslayer(IP):
 		srcip = packet[IP].src
 		dstip = packet[IP].dst
-		if packet.haslayer(TCP):
-			print(f"{srcip}:{packet[TCP].sport} --------> {dstip}:{packet[TCP].dport} on TCP")
+		if packet.haslayer(TCP) and showttl != "y":
+			print(f"\033[1;34m[+]|{srcip}:{packet[TCP].sport}|\033[1;34m      -------->      \033[1;33m|{dstip}:{packet[TCP].dport}|TTL:{showttl(packet)}| on TCP\033[1;33m")
+			flagging(packet)
 		elif packet.haslayer(UDP):
-			print(f"{srcip}:{packet[UDP].sport} --------> {dstip}:{packet[UDP].dport} on UDP")
+			print(f"\033[0;36m[+]|{srcip}:{packet[UDP].sport}|      -------->      |{dstip}:{packet[UDP].dport}|  on UDP\033[0;36m")
 		
-sniff(prn=inspect, store=0, iface="wlan0", count=100)
-
+sniff(prn=inspect, store=0, iface="wlan0", count=1000000)
