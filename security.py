70fc7097dcb82918f8aec703daa28d886f7a51a0
LFTE-guy <LFTE@proton.me>
2026-08-09 14:32:41 -0400

    Refactor packet scanning logic in security.py
    Refactored the scan function into sys and added local IP and trap ports handling.

--- security.py
+++ security.py
@@ -1,15 +1,42 @@
-from scapy.all import sniff, IP, TCP, Raw,scapy
-from scapy.all import sniff, IP, TCP, Raw,scapy
-def scan(packet):
-	if packet.haslayer(TCP):
-		flags = int(packet[TCP].flags)
-		if flags == 0:
-			return  "\033[1;31mNULL SCAN ALERT\033[0m"
-		elif flags == 1:
-			return"\033[1;31m ILLEGAL FIN SCAN ALERT\033[0m"
-		elif flags == 41:
-			return"\033[1;31m XMAS SCAN ALERT\033[0m"
-		elif flags == 3:
-			return"\033[1;31m SYN-FIN SCAN ALERT\033[0m"
+from scapy.all import sniff, IP, TCP, Raw,scapy,conf,get_if_addr
+localip = get_if_addr(conf.iface)
+trapports = (333,443,80,22,23,26,30,118,113)	
+def sys(packet):
+	
+	if packet.haslayer(IP):
+		
+		if packet.haslayer(TCP):
+			flags = int(packet[TCP].flags)
+			
+			
+			if flags == 0:
+				return  "\033[1;31mNULL SCAN ALERT\033[0m"
+				
+			elif flags == 1:
+				return"\033[1;31m ILLEGAL FIN SCAN ALERT\033[0m"
+				
+			elif flags == 41:
+				return"\033[1;31m XMAS SCAN ALERT\033[0m"
+				
+			elif flags == 3:
+				return"\033[1;31m SYN-FIN SCAN ALERT\033[0m"
+				
+			elif flags == 2 and not packet[TCP].options :
+				return "empty TCP options!!"
+			
+			elif packet[IP].dst == localip:	
+				
+				trapports = (333,443,80,22,23,26,30,118,113)	
+				
+				if packet[TCP].dport in trapports:
+					
+					return("TRAP PORT PROBED")
+					
+				else: 
+					return("Normal")
+			else:
+				return"Normal"
 		else:
 			return("Normal")
+	else:
+		return("Normal")
