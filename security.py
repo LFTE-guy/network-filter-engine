dc86a4ae69b034da7680e61ed9218f87e473059d
LFTE-guy <LFTE@proton.me>
2026-08-14 17:56:05 -0400

    Refactor sys function for TCP packet analysis

--- security.py
+++ security.py
@@ -1,35 +1,47 @@
 from scapy.all import sniff, IP, TCP, Raw,scapy,conf,get_if_addr
+from layout import colors
 localip = get_if_addr(conf.iface)
 trapports = (333,443,80,22,23,26,30,118,113)
-anomlyflags= (0,1,41,3)	
+anomlyflags= (0,1,41,3)
 def sys(packet):
-	
-	if packet.haslayer(IP):
+	nwindow = packet[TCP].window
+	if packet.haslayer(IP) and packet.haslayer(TCP):
+		if packet[IP].flags.MF == 1 or packet[IP].frag > 0 :
+				return("fragmentation detected!!")
+		flags = int(packet[TCP].flags)
 		for name, value in packet[TCP].options:
-				if name == "MSS" and value == 1024:
-					return("MSS anomly!!")
-		if packet.haslayer(TCP):
-			flags = int(packet[TCP].flags)
-			
-			
-			if flags in anomlyflags :
-				return  (f"SCAN ALERT - we are being scanned")
-			elif flags == 2 and not bool(packet.options):
-				return ("syn without options!!- likely a scan")
-			
-			elif packet[IP].dst == localip:	
+			if name == "MSS" and value <= 1024 and nwindow <= 4096:
+				return("MSS and window size anomly!!")
+		
+		if flags in anomlyflags :
+			return  (f"{colors.RED}SCAN ALERT - we are being scanned{colors.END}")
 				
-				trapports = (333,443,80,22,23,26,30,118,113)	
+		elif flags in (32,4,2) and len(packet[TCP].payload) > 0:
+			return("payload+flag anomly!!")
+			
+			
+		elif flags == 2 and not bool(packet.options) and nwindow <= 4096  :
+			return ("syn without options!!- likely a scan")
+			
+		elif packet[IP].dst == localip:	
 				
-				if packet[TCP].dport in trapports:
+			trapports = (333,443,80,22,23,26,30,118,113)	
+				
+			if packet[TCP].dport in trapports:
 					
-					return("TRAP PORT PROBED")
+				return("TRAP PORT PROBED")
 					
-				else: 
-					return("Normal")
-			else:
-				return"Normal"
+			else: 
+				return("Normal")
 		else:
-			return("Normal")
+			return"Normal"
 	else:
 		return("Normal")
+def ntcpsys(packet):
+	if packet.haslayer(IP):
+		if packet[IP].flags.MF == 1 or packet[IP].frag > 0 :
+				return("fragmentation detected!!")
+		else:
+			return("normal")
+	else:
+		return("test")
